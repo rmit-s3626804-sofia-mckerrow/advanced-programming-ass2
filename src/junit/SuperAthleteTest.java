93414d22b292b62047d0a0a0d9166c6297f19b9a
Sofia_M <s3626804@student.rmit.edu.au>
2017-07-23 17:56:02 +1000

    Added SuperAthleteTest to junit package

---
+++ src/junit/SuperAthleteTest.java
@@ -0,0 +1,52 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import customExceptions.WrongTypeException;
+import gameComponents.Cycle;
+import gameComponents.Run;
+import gameComponents.SuperAthlete;
+import gameComponents.Swim;
+
+public class SuperAthleteTest {
+
+	SuperAthlete superAthlete;
+	Cycle cycle;
+	Run run;
+	Swim swim;
+
+	@Before
+	public void setUp() throws Exception {
+		superAthlete = new SuperAthlete("s111", "Test SuperAthlete", "SuperAthlete", 20, "SA");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	// Check if a superathlete can be added to a Cycle game
+	// Expected outcome is that the test will pass
+	@Test
+	public void testCanRaceInGame1() throws WrongTypeException {
+		assertTrue(superAthlete.canRaceInGame(cycle));
+	}
+
+	// Check if a cyclist can be added to a Run game
+	// Expected outcome is that the test will pass
+	@Test
+	public void testCanRaceInGame2() throws WrongTypeException {
+		assertTrue(superAthlete.canRaceInGame(run));
+	}
+
+	// Check if a cyclist can be added to a Swim game
+	// Expected outcome is that the test will pass
+	@Test
+	public void testCanRaceInGame3() throws WrongTypeException {
+		assertTrue(superAthlete.canRaceInGame(swim));
+	}
+
+}
