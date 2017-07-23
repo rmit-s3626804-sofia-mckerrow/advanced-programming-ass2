5f8fe6af2c7e7d22f2131d92c07272ef406a4c89
Sofia_M <s3626804@student.rmit.edu.au>
2017-07-23 18:05:45 +1000

    Added SwimmerTest to junit package

--- src/gui/SelectGameController.java
+++ src/gui/SelectGameController.java
@@ -5,7 +5,6 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import gameComponents.Game;
@@ -27,7 +26,6 @@ public class SelectGameController implements Initializable{
 	DataBase thisDB = new DataBase();
 	Game myGame;
 	private String raceType;
-	private ArrayList<Game> games = new ArrayList<Game>();
 	
 	@FXML
 	private Label addGameStatus;
---
+++ src/junit/SwimmerTest.java
@@ -0,0 +1,55 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import customExceptions.WrongTypeException;
+import gameComponents.Cycle;
+import gameComponents.Run;
+import gameComponents.Swim;
+import gameComponents.Swimmer;
+
+public class SwimmerTest {
+
+	Swimmer swimmer;
+	Cycle cycle;
+	Run run;
+	Swim swim;
+
+	@Before
+	public void setUp() throws Exception {
+		swimmer = new Swimmer("s111", "Test Swimmer", "Swimmer", 20, "SA");
+		cycle = new Cycle("c01");
+		run = new Run("r01");
+		swim = new Swim("s01");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	// Check if a swimmer can be added to a Cycle game
+	// Expected outcome is that the test will pass
+	@Test
+	public void testCanRaceInGame1() throws WrongTypeException {
+		assertFalse(swimmer.canRaceInGame(cycle));
+	}
+
+	// Check if a swimmer can be added to a Run game
+	// Expected outcome is that the test will pass
+	@Test
+	public void testCanRaceInGame2() throws WrongTypeException {
+		assertFalse(swimmer.canRaceInGame(run));
+	}
+
+	// Check if a swimmer can be added to a Swim game
+	// Expected outcome is that the test will pass
+	@Test
+	public void testCanRaceInGame3() throws WrongTypeException {
+		assertTrue(swimmer.canRaceInGame(swim));
+	}
+
+}
